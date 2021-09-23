197cb731784bae0178acb31e1c1499037c5d6610
Luan Almeida <luanlmd@gmail.com>
2021-09-23 17:15:08 -0300

    fix typo

--- 1-cpf/cpf.js
+++ 1-cpf/cpf.js
@@ -1,4 +1,4 @@
-const calculateVeryfyingDigit = (cpf, digit) => {
+const calculateVerifyingDigit = (cpf, digit) => {
     const multiplierStart = (digit === 1)? 10 : 11;
     const calculateUntil = (digit === 1)? 9 : 10;
 
@@ -18,13 +18,13 @@ const validate = (cpf) => {
 
     cpf = cpf.replace(/[^0-9]/g, '')
     if (cpf.length != 11) { return false }
-    
+
     if (cpf.split("").every(c => c === cpf[0])) { return false }
 
-    const firstVerifyingDigit = calculateVeryfyingDigit(cpf, 1);
+    const firstVerifyingDigit = calculateVerifyingDigit(cpf, 1);
     if (firstVerifyingDigit != cpf[9]) { return false; }
 
-    const secondVerifyingDigit = calculateVeryfyingDigit(cpf, 2);
+    const secondVerifyingDigit = calculateVerifyingDigit(cpf, 2);
     if (secondVerifyingDigit != cpf[10]) { return false; }
 
     return true;

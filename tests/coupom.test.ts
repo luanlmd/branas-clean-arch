72ebf46b14b309d6a23b34d40c96076b012e598c
Luan Almeida <luanlmd@gmail.com>
2021-10-04 19:29:53 -0300

    add 1 test

--- tests/coupom.test.ts
+++ tests/coupom.test.ts
@@ -7,7 +7,13 @@ test ('Coupom should be created', () => {
 })
 
 test('Coupom should be expired', () => {
-    const expiredDate = new Date(Date.now() - 1)
-    const coupom = new Coupom('code', 10, expiredDate)
+    const expirationDate = new Date(Date.now() - 1)
+    const coupom = new Coupom('code', 10, expirationDate)
     expect(coupom.isExpired()).toBeTruthy()
+})
+
+test('Coupom should not be expired', () => {
+    const expirationDate = new Date(Date.now() + 10)
+    const coupom = new Coupom('code', 10, expirationDate)
+    expect(coupom.isExpired()).toBeFalsy()
 })
